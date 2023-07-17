0784719cc41b813e20f65c44b77a68abf0d957fe
prydin <prydin@vmware.com>
2023-07-17 10:14:31 -0400

    Fixed deletion bugs

--- src/main/python/delete.py
+++ src/main/python/delete.py
@@ -8,8 +8,8 @@ def handler(context, inputs):
     del new_inputs["parameters"]
     new_inputs["variables"] = {
         "tfm.airid": {"value": inputs["parameters"]["airid"]},
-        "tfm.project_name": {"value": inputs["parematers"]["project_name"]},
-        "tfm.service_name": inputs["serviceName"]
+        "tfm.project_name": {"value": inputs["parameters"]["project_name"]},
+        "tfm.service_name": { "value": inputs["serviceName"]}
     }
     return run_pipeline(context, new_inputs, inputs["deletionPipeline"])
     return inputs # Yes, inputs! We want to return the original data
